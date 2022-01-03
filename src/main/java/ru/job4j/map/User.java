8a2df116b9e91a9c4ed5b1837a072c5327a0367b
Vladimir Likhachev <lettervl@ya.ru>
2022-01-03 17:57:26 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,10 @@
 package ru.job4j.map;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Vladimir Likhachev
  */
@@ -14,5 +18,16 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>();
+        User user1 = new User("Ivan", 1, Calendar.getInstance());
+        User user2 = new User("Ivan", 1, Calendar.getInstance());
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        for (User user : map.keySet()) {
+            System.out.println("user = " + user);
+        }
     }
 }
