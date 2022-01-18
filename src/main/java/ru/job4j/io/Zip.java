9bc86a39e406c76f5c43571edff73265ca74c206
AkiBoba <lettervl@ya.ru>
2022-01-18 12:14:13 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,6 +3,8 @@ package ru.job4j.io;
 import ru.job4j.list.List;
 
 import java.io.*;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -12,9 +14,14 @@ public class Zip {
     }
 
     public static void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+        if (!source.exists()) {
+
+        }
+        try (ZipOutputStream zip = new ZipOutputStream(new
+                BufferedOutputStream(new FileOutputStream(target)))) {
             zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+            try (BufferedInputStream out = new BufferedInputStream(new
+                    FileInputStream(source))) {
                 zip.write(out.readAllBytes());
             }
         } catch (Exception e) {
@@ -22,7 +29,26 @@ public class Zip {
         }
     }
 
+    private static boolean validate(String[] args) {
+        boolean result = true;
+        if (args.length == 0) {
+            return false;
+        }
+        String regex = "^-[^=]+=[^=]+";
+        Predicate<String> filter = line -> Pattern.matches(regex, line);
+        for (String str : args) {
+            if (!filter.test(str)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
+        if (!validate(args)) {
+            throw new IllegalArgumentException();
+        }
         ArgsName argsName = ArgsName.of(new String[] {args[0], args[2]});
         String arg1 = argsName.get("d");
         String arg2 = argsName.get("o");
