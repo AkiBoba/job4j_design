e147bbfda10f2f841d4a1bb55678e87313cf53cb
AkiBoba <lettervl@ya.ru>
2022-01-19 13:37:36 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,39 +1,30 @@
 package ru.job4j.io;
 
-import ru.job4j.list.List;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-import java.io.File;
-
-import static ru.job4j.io.Search.search;
 
 public class Zip {
-    public static void packFiles(List<Path> sources, File target) {
-        if (sources.size() > 1) {
-            sources.forEach(s ->  packSingleFile(s.toAbsolutePath(), target));
+
+    public static void packFiles(List<String> sources, File target) {
+//        sources.forEach(s ->  packSingleFile(Paths.get(s).toFile(), target));
+        File file;
+        for (String str : sources) {
+            file = Paths.get(str).toFile();
+            packSingleFile(file, target);
         }
-        sources.forEach(s -> packSingleFile(s.toAbsolutePath(), target));
     }
 
-    public static void packSingleFile(Path source, File target) {
-
-        try (ZipOutputStream zip = new ZipOutputStream(new
-                BufferedOutputStream(new FileOutputStream(target)))) {
-
-            zip.putNextEntry(new ZipEntry(source.toString()));
-
-            try (BufferedInputStream out = new BufferedInputStream(new
-                    FileInputStream(source.toString()))) {
-
+    public static void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
                 zip.write(out.readAllBytes());
             }
         } catch (Exception e) {
@@ -41,14 +32,14 @@ public class Zip {
         }
     }
 
-       static List<Path> excludeFiles(Path root, String exclude) throws IOException {
-        Predicate<Path> condition = name -> !name.endsWith(exclude);
-        List<Path> list = new ArrayList<>();
-            list.addAll(search(root, condition));
-        return (List<Path>) list;
+    public static List<String> excludeFiles(Path root, String exclude) throws IOException {
+        List<String> list = new ArrayList<>();
+        Predicate<Path> condition = name -> !name.toString() .endsWith(exclude);
+        Search.search(root, condition).forEach(p -> list.add(p.toString()));
+        return list;
     }
 
-    public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {
 
         if (args.length < 3) {
             throw new IllegalArgumentException();
@@ -57,9 +48,6 @@ public class Zip {
         String arg1 = argsName.get("d");
         String arg2 = argsName.get("o");
         String arg3 = argsName.get("e");
-        packFiles(
-                excludeFiles(Paths.get(arg1), arg3),
-                new File(arg2)
-        );
+        packFiles(excludeFiles(Paths.get(arg1), arg3), Paths.get(arg1).toFile());
     }
 }
