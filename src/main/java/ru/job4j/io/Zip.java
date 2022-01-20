5059cdcc8a775f271d7dcff06386a52c23dbc3c2
AkiBoba <lettervl@ya.ru>
2022-01-20 18:00:29 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -13,7 +13,7 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public static void packFiles(List<String> sources, File target) {
-        sources.forEach(s ->  packSingleFile(Paths.get(s).toFile(), target));
+        sources.forEach(s -> packSingleFile(Paths.get(s).toFile(), target));
     }
 
     public static void packSingleFile(File source, File target) {
@@ -29,30 +29,26 @@ public class Zip {
 
     public static List<String> excludeFiles(Path root, String exclude) throws IOException {
         List<String> list = new ArrayList<>();
-        Predicate<Path> condition = name -> !name.toString() .endsWith(exclude);
+        Predicate<Path> condition = name -> !name.toString().endsWith(exclude);
         Search.search(root, condition).forEach(p -> list.add(p.toString()));
         return list;
     }
 
-    public static boolean validation(File start, File out) {
-        boolean result = start.exists() || out.exists();
-        if (!start.isDirectory() && !out.isDirectory()) {
-            result = false;
-        }
-        return result;
+    public static boolean validation(File start) {
+        return start.exists() && start.isDirectory() && start.isDirectory();
     }
 
         public static void main(String[] args) throws IOException {
 
-        if (args.length < 3) {
+        if (args.length != 3) {
             throw new IllegalArgumentException();
         }
         ArgsName argsName = ArgsName.of(new String[] {args[0], args[1], args[2]});
-        String arg1 = argsName.get("d");
-        String arg2 = argsName.get("o");
-        String arg3 = argsName.get("e");
-        if (validation(Paths.get(arg1).toFile(), Paths.get(arg2).toFile())) {
-            packFiles(excludeFiles(Paths.get(arg1), arg3), Paths.get(arg2).toFile());
+        String dirForZip = argsName.get("d");
+        String zipDirOut = argsName.get("o");
+        String exclude = argsName.get("e");
+        if (validation(Paths.get(dirForZip).toFile())) {
+            packFiles(excludeFiles(Paths.get(dirForZip), exclude), Paths.get(zipDirOut).toFile());
         }
     }
 }
