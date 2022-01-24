430dde5e1701d10459abb2d266a03e20c0743010
AkiBoba <lettervl@ya.ru>
2022-01-24 17:35:58 +0300

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -2,47 +2,69 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * @author Vladimir Likhachev
  */
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
+        validation(argsName);
+        String delimiter = argsName.get("delimiter");
         File file = new File(argsName.get("path"));
         Scanner scanner = new Scanner(file);
         String string = scanner.nextLine();
         List<String> result = new ArrayList<>();
-        List<String> list = Arrays.stream(string.split(argsName.get("delimiter"))).toList();
+        List<String> list = Arrays.stream(string.split(delimiter)).toList();
         List<String> filters = Arrays.stream(argsName.get("filter").split(",")).toList();
-        result.add(string.split(argsName.get("delimiter"))[list.indexOf(filters.get(0))]);
-        result.add(";");
-        result.add(string.split(argsName.get("delimiter"))[list.indexOf(filters.get(1))]);
-        result.add(System.lineSeparator());
+        result.add(newString(list, string, delimiter, filters));
         while (scanner.hasNext()) {
             string = scanner.nextLine();
-            result.add(string.split(argsName.get("delimiter"))[list.indexOf(filters.get(0))]);
-            result.add(";");
-            result.add(string.split(argsName.get("delimiter"))[list.indexOf(filters.get(1))]);
-            result.add(System.lineSeparator());
+            result.add(newString(list, string, delimiter, filters));
         }
-//        BufferedWriter bw = new BufferedWriter(new FileWriter(argsName.get("out")));
-//        for (String str : result) {
-//            try {
-//                bw.write(str);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-        BufferedOutputStream bs = new BufferedOutputStream(new FileOutputStream(argsName.get("out")));
-        for (String str : result) {
-            try {
-                bs.write(str.getBytes(StandardCharsets.UTF_8));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+        String data = String.join(
+                System.lineSeparator(),
+                result.stream().toList()
+        );
+
+        if (argsName.get("out").equals("stdout")) {
+            System.out.println(data);
         }
-        result.forEach(System.out :: print);
+        try {
+            Files.writeString(new File(argsName.get("out")).toPath(), data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    private static String newString(List<String> list, String string, String delimiter, List<String> filters) {
+        String newstring = "";
+        for (int index = 0; index < filters.size(); index++) {
+            newstring = newstring.concat(string.split(delimiter)[list.indexOf(filters.get(index))] + delimiter);
+        }
+        return newstring.substring(0, newstring.length() - 1);
+    }
+
+    public static void validation(ArgsName args) {
+        String res = args.get("path");
+        if (!Paths.get(res).toFile().exists()) {
+            throw new IllegalArgumentException("Файл с данными не существует");
+        }
+        if (!Paths.get(args.get("out")).toFile().exists() && !args.get("out").equals("stdout")) {
+            throw new IllegalArgumentException("Ключ out имеет недопустимые значения");
+        }
+        if (!args.get("delimiter").equals(";")) {
+            throw new IllegalArgumentException("Ключ delimiter имеет недопустимые значения");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ArgsName argsName = ArgsName.of(args);
+        validation(argsName);
+        handle(argsName);
+    }
 }
--- src/test/java/ru/job4j/io/CSVReaderTest.java
+++ src/test/java/ru/job4j/io/CSVReaderTest.java
@@ -7,6 +7,7 @@ import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Scanner;
 
 /**
  * @author Vladimir Likhachev
@@ -37,9 +38,34 @@ public class CSVReaderTest {
                 "Tom;20",
                 "Jack;25",
                 "William;30"
-        ).concat(System.lineSeparator());
+        );
         CSVReader.handle(argsName);
-//        Assert.assertEquals(expected, Files.readString(target.toPath()));
+        Assert.assertEquals(expected, Files.readString(target.toPath()));
     }
 
+    @Test
+    public void whenFilterThreeColumns() throws Exception {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = temporaryFolder.newFile("source.csv");
+        File target = temporaryFolder.newFile("target.csv");
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(), "-filter=name,age,education"
+        });
+        Files.writeString(file.toPath(), data);
+        String expected = String.join(
+                System.lineSeparator(),
+                "name;age;education",
+                "Tom;20;Bachelor",
+                "Jack;25;Undergraduate",
+                "William;30;Secondary special"
+        );
+        CSVReader.handle(argsName);
+        Assert.assertEquals(expected, Files.readString(target.toPath()));
+    }
 }
