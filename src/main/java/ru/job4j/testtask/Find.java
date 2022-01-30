9d2cdfad63e6d01720e967ea9bc563e45f309607
Vladimir Likhachev <lettervl@ya.ru>
2022-01-30 11:44:48 +0300

    2. Поиск файлов по критерию [#783]

--- src/main/java/ru/job4j/testtask/Find.java
+++ src/main/java/ru/job4j/testtask/Find.java
@@ -1,17 +1,50 @@
 package ru.job4j.testtask;
 
 import ru.job4j.io.ArgsName;
+import ru.job4j.io.Search;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 import static ru.job4j.io.Search.search;
 
 public class Find {
+
+    public static List<String> excludeFiles(Path root, String name, String typeFind) throws IOException {
+        List<String> list = new ArrayList<>();
+        Predicate<Path> filter = str -> name.equals(str.toFile().getName());
+        if ("mask".equals(typeFind)) {
+            String regex = "^" + name.replace(".", "\\.")
+                    .replace("*", ".*")
+                    .replace("?", ".");
+            filter = line -> Pattern.matches(regex, line.toFile().getName());
+        }
+        if ("regex".equals(typeFind)) {
+            String regex = name;
+            filter = line -> Pattern.matches(regex, line.toFile().getName());
+        }
+
+        Search.search(root, filter).forEach(p -> list.add(p.toString()));
+        return list;
+    }
+
+    public static void saveLog(List<String> log, String path) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251")))) {
+            log.forEach(pw :: println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void validation(String[] args) {
         if (args.length != 4) {
             throw new IllegalArgumentException("Недостаточно аргументов для корректной работы приложения");
@@ -30,22 +63,25 @@ public class Find {
 
     public static void main(String[] args) throws IOException {
         validation(args);
-        String regex = "";
         ArgsName argsName = ArgsName.of(args);
         String dirForFind = argsName.get("d");
         String name = argsName.get("n");
         String typeFind = argsName.get("t");
         String dirForSave = argsName.get("o");
-        if ("regex".equals(typeFind)) {
-            regex = "^" + name.replace(".", "\\.")
-                    .replace("*", ".*")
-                    .replace("?", ".");
-        }
-        String finalRegex = regex;
-        Predicate<String> filter = line -> Pattern.matches(finalRegex, line);
-        String tmp = "txt.txt";
-        if (filter.test(tmp)) {
-            System.out.println(tmp);
-        }
+
+        saveLog(excludeFiles(Paths.get(dirForFind), name, typeFind), dirForSave);
+
+//
+//        if (!"name".equals(typeFind)) {
+//            regex = "^" + name.replace(".", "\\.")
+//                    .replace("*", ".*")
+//                    .replace("?", ".");
+//        }
+//        String finalRegex = regex;
+//        Predicate<String> filter = line -> Pattern.matches(finalRegex, line);
+//        String tmp = "txt.txt";
+//        if (filter.test(tmp)) {
+//            System.out.println(tmp);
+//        }
     }
 }
