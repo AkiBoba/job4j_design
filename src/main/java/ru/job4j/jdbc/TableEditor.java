bdf60d40c524cf2a4e54ff851d0f55797c4ec587
Vladimir Likhachev <lettervl@ya.ru>
2022-02-06 22:04:14 +0300

    0.1. Statement [#379306]

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,81 @@
+package ru.job4j.jdbc;
+
+import java.io.File;
+import java.io.FileReader;
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+/**
+ * @author Vladimir Likhachev
+ */
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() {
+//        connection = null;
+//        Class.forName("org.postgresql.Driver");
+//        String url = "jdbc:postgresql://localhost:5432/idea_db";
+//        String login = "postgres";
+//        String password = "password";
+//        return DriverManager.getConnection(url, login, password);
+    }
+
+    public void createTable(String tableName) {
+
+    }
+
+    public void dropTable(String tableName) {
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+    }
+
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "select * from %s limit 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        properties.load(new FileReader("app.properties"));
+//        String drv = properties.get("hibernate.connection.driver_class").toString();
+    }
+}
